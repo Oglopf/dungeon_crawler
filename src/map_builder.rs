1d175f22d0331c898010a039d4d5984405ed592b
Travis Ravert <trav271@gmail.com>
2024-07-25 22:19:38 -0700

    Adds fill method to mapbuilder.

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -7,6 +7,17 @@ const NUMS_ROOMS: usize = 20;
 // i.e. where player begins.
 pub struct MapBuilder {
     pub map: Map,
+    // Rect comes from bracker-lib.
+    // helper function for calcs w/ rectangles.
     pub rooms: Vec<Rect>,
     pub player_start: Point,
+}
+
+impl MapBuilder {
+    pub fn fill(&mut self, tile: TileType) {
+        self.map
+            .tiles
+            .iter_mut() // gives a mutable iterator. 
+            .for_each( |t| *t = tile); // deref t to write to the reference of 
+    }
 }
