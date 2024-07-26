39996db553500d0147858ab574a17f779d897a25
Travis Ravert <trav271@gmail.com>
2024-07-25 22:30:38 -0700

    Adds functionality to MapBuilder to geenrate random rooms

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -20,4 +20,31 @@ impl MapBuilder {
             .iter_mut() // gives a mutable iterator. 
             .for_each( |t| *t = tile); // deref t to write to the reference of 
     }
+
+    pub fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
+        while self.rooms.len() > NUMS_ROOMS {
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10), 
+                rng.range(1, SCREEN_WIDTH - 10), 
+                rng.range(2, 10), 
+                rng.range(2, 10)
+            );
+
+            let mut overlap = false;
+            for r in self.rooms.iter() {
+                if r.intersect(&room) {
+                    overlap = true;
+                }
+            }
+            if !overlap {
+                room.for_each(|p| {
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        self.map.tiles[idx] = TileType::Floor;
+                    }
+                });
+                self.rooms.push(room)
+            }
+        }
+    }
 }
