7205a3222b74ac5ce8a6a5cf209ba3f92de68aac
Travis Ravert <trav271@gmail.com>
2024-07-25 22:11:51 -0700

    adds map builder struct

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -3,6 +3,10 @@ use crate::prelude::*;
 
 const NUMS_ROOMS: usize = 20;
 
+// Build map with map, rooms, and origin 
+// i.e. where player begins.
 pub struct MapBuilder {
-    
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
 }
